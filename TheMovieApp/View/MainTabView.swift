24c7b4cbe7aa2826807a498bc9c01bbe82f97115
Virginia Lombardi <virginialombardii@gmail.com>
2024-08-19 12:04:59 -0300

    Cleaning code.

--- TheMovieApp/View/MainTabView.swift
+++ TheMovieApp/View/MainTabView.swift
@@ -5,33 +5,6 @@
 //  Created by Virginia Lombardi on 18/8/24.
 //
 
-//import SwiftUI
-//
-//struct MainTabView: View {
-//    var body: some View {
-//        TabView {
-//            MoviesView()
-//                .tabItem {
-//                    Image(systemName: "play.house")
-//                }
-//            
-//            SearchMoviesView()
-//                .tabItem {
-//                    Image(systemName: "magnifyingglass")
-//                }
-//            
-//            FavoritesView()
-//                .tabItem {
-//                    Image(systemName: "suit.heart")
-//                }
-//        }
-//    }
-//}
-//
-//#Preview {
-//    MainTabView()
-//}
-
 import SwiftUI
 
 struct MainTabView: View {
